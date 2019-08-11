490572bf735e7a3c185465374c594066301d49b2
youarenomatch <876355950@qq.com>
2019-08-11 22:00:18 +0800

    管理中心

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about container">
+      你好世界
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
